c6d1b04f7da609b7cd60be84e40cc69843590a45
Crissy <crystalgrant82@gmail.comgit config --global user.email crystalgrant82@gmail.com>
2018-08-10 04:08:49 -0400

    social links, fixed responsive navbar

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,19 +6,36 @@ import { Button } from './Button';
 
 const Nav = () =>(
 
-  <div className="pos-f-t">
-<div className="collapse" id="navbarToggleExternalContent">
-  <div className="bg-dark p-4">
-    <h4 className="text-white">Collapsed content</h4>
-    <span className="text-muted">Toggleable via the navbar brand.</span>
-  </div>
-</div>
-<nav className="navbar navbar-dark bg-dark">
-  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-</nav>
-</div>
+    <nav className='navbar navbar-expand-md navbar-light bg-light ' >
+    <div className="container-fluid">
+    <div className="navbar-header">
+      <Link to='/' className='navbar-brand'>Home</Link>
+
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+        <span className='navbar-toggler-icon'/>
+      </button>
+    </div>
+        <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+            <li className='nav-item'>
+              <Link to='/about' className='nav-link'> About</Link>
+            </li>
+            {" "}
+            <li className='nav-item'>
+              <Link to='/gallery' className='nav-link'> Gallery</Link>
+            </li>
+            {" "}
+            <li className='nav-item'>
+              <Link to='playground' className='nav-link'> Playground</Link>
+            </li>
+            {" "}
+            <li className='nav-item'>
+              <Link to='/contact' className='nav-link'> Contact</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
 
 )
 
--- src/Components/SocialLinks.js
+++ src/Components/SocialLinks.js
@@ -1,21 +1,28 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import facebook from '../images/facebook.png'
+import instagram from '../images/instagram.png'
+import github from '../images/github.png'
+import twitter from '../images/twitter.png'
+
 const SocialLinks = () => (
-  <nav className='container'>
-    <ul className='navbar'>
-      <li>
-        <Link to='https://www.facebook.com/CrystalGrantCodester'>Facebook</Link>
-      </li>
-      <li>
-        <Link to='https://www.instagram.com/crissy_love82/'>Instagram</Link>
-      </li>
-      <li>
-        <Link to='https://github.com/crissygrant82'>Github</Link>
-      </li>
-      <li>
-        <Link to='https://twitter.com/CrissyCodester'>Twitter</Link>
-      </li>
-    </ul>
+  <nav className='nav justify-content-center'>
+        <Link
+        className='nav-link'
+        to='https://www.facebook.com/CrystalGrantCodester'><img src={facebook} alt="facebook"/></Link>
+
+        <Link
+        className='nav-link'
+        to='https://www.instagram.com/crissy_love82/'><img src={instagram} alt="instagram"/></Link>
+
+        <Link
+        className='nav-link'
+        to='https://github.com/crissygrant82'><img src={github} alt="github"/></Link>
+
+        <Link
+        className='nav-link'
+        to='https://twitter.com/CrissyCodester'><img src={twitter} alt="twitter"/></Link>
+
   </nav>
 )
 
